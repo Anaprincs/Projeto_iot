f01541c6b8d4948a116afd2e80fc14e89bb01908
Anaprincs <hipolitocarolinaana@gmail.com>
2025-08-12 15:44:58 -0300

    criamos e eitamos as models e migrations

---
+++ app/Models/Sensor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sensor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'ambiente_id',
+        'codigo',
+        'tipo'
+    ];
+public function registos(){
+
+return $this->hasMany(Registro::class);
+}
+ public function ambiente(){
+    return $this->belongsTo(Ambiente::class);
+ }
+
+}
