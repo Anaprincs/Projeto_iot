7911b4125b6008fedf63ce14ccd3445c49220548
Anaprincs <hipolitocarolinaana@gmail.com>
2025-08-14 15:50:07 -0300

    eu editei o create, falta fazer a personalização

---
+++ app/Livewire/Ambiente/Ambientelist.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Ambiente;
+
+use Livewire\Component;
+
+class Ambientelist extends Component
+{
+    public function render()
+    {
+        return view('livewire.ambiente.ambientelist');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Livewire\Ambiente\AmbienteCreate;
+use App\Models\Ambiente;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('ambiente/create', AmbienteCreate::class);
