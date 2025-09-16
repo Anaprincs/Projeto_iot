20c64b8ef4b04f737c7298fcc603742f417782b2
Anaprincs <hipolitocarolinaana@gmailcom>
2025-09-16 15:24:20 -0300

    Eu criei o controller e form request , e fiz um index

---
+++ app/Http/Requests/RegistroRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegistroRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'sensor_id' => 'required',
+            'valor' => 'required',
+            'unidade' => 'required',
+            'data_hora' => 'required'
+        ];
+    }
+
+public function messages()
+{
+    return[
+        'sensor.required' => 'Este campo é obrigatorio',
+        'valor.required' => 'Este campo é obrigatorio',
+        'unidade.required' => 'Este campo é obrigatorio',
+        'data_hora.required' => 'Este campo é obrigatorio',
+    ];
+}
+}
---
+++ app/Livewire/Registro/RegistroIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Registro;
+
+use Livewire\Component;
+
+class RegistroIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.registro.registro-index');
+    }
+}
